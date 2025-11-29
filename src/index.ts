ad795489283b6c346159fc0dc3856f0300cd65a6
Ezana Tadesse <ezanatadesse2003@gmail.com>
2025-11-29 18:34:08 +0300

    Initialized express server with joi validation.

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/register.routes";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", authRoutes);
+
+// Test route
+app.get("/", (req, res) => {
+  res.json({ message: "GGE Authenticator API is running" });
+});
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
