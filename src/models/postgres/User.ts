ff39bd0e6a28137e2849c10642f805ceaa35c3ed
Ezana Tadesse <ezanatadesse2003@gmail.com>
2025-11-29 19:51:31 +0300

    Added models for sequelize.

---
+++ src/models/postgres/User.ts
@@ -0,0 +1,57 @@
+// src/models/User.ts
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  Default,
+  CreatedAt,
+  UpdatedAt,
+  Unique,
+  AllowNull,
+  IsEmail,
+} from "sequelize-typescript";
+import { v4 as uuidv4 } from "uuid";
+
+@Table({
+  tableName: "users",
+  timestamps: true,
+})
+export class User extends Model<User> {
+
+  @PrimaryKey
+  @Default(uuidv4)
+  @Column({ type: DataType.UUID })
+  id!: string;
+
+  @Unique
+  @IsEmail
+  @AllowNull(false)
+  @Column({ type: DataType.STRING })
+  email!: string;
+
+  @AllowNull(true)
+  @Column({ type: DataType.STRING })
+  username?: string;
+
+  @AllowNull(true)
+  @Column({ type: DataType.TEXT })
+  passwordHash?: string;
+
+  @AllowNull(true)
+  @Column({ type: DataType.STRING })
+  phone?: string;
+
+  @AllowNull(true)
+  @Column({ type: DataType.STRING })
+  avatarUrl?: string;
+
+  @CreatedAt
+  @Column({ field: "created_at" })
+  createdAt!: Date;
+
+  @UpdatedAt
+  @Column({ field: "updated_at" })
+  updatedAt!: Date;
+}
---
+++ src/models/postgres/UserApp.ts
@@ -0,0 +1,56 @@
+// src/models/UserApp.ts
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  Default,
+  ForeignKey,
+  CreatedAt,
+  BelongsTo,
+  AllowNull,
+  Unique,
+} from "sequelize-typescript";
+import { v4 as uuidv4 } from "uuid";
+import { User } from "./User";
+import { App } from "./App";
+
+@Table({
+  tableName: "user_apps",
+  timestamps: true,
+  indexes: [
+    {
+      unique: true,
+      fields: ["user_id", "app_id"],
+      name: "unique_user_app",
+    },
+  ],
+})
+export class UserApp extends Model<UserApp> {
+
+  @PrimaryKey
+  @Default(uuidv4)
+  @Column({ type: DataType.UUID })
+  id!: string;
+
+  @ForeignKey(() => User)
+  @AllowNull(false)
+  @Column({ type: DataType.UUID })
+  userId!: string;
+
+  @ForeignKey(() => App)
+  @AllowNull(false)
+  @Column({ type: DataType.UUID })
+  appId!: string;
+
+  @BelongsTo(() => User)
+  user!: User;
+
+  @BelongsTo(() => App)
+  app!: App;
+
+  @CreatedAt
+  @Column({ field: "created_at" })
+  createdAt!: Date;
+}
