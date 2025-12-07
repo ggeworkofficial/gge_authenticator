f0ced49193d6bf080007f236e46e7abff80de5ad
Ezana Tadesse <ezanatadesse2003@gmail.com>
2025-12-07 11:29:13 +0300

    Refactor session creation to use updateOne with upsert for better handling of existing sessions

--- src/repositories/session.repository.ts
+++ src/repositories/session.repository.ts
@@ -19,16 +19,24 @@ export class SessionRepository {
     try {
       
       const now = new Date();
-      const toInsert = {
-        ...doc,
-        createdAt: doc.createdAt || now,
-        expiresAt: doc.expiresAt || doc.refreshTokenExpiresAt,
+      const update = {
+        $set: {
+          ...doc,
+          createdAt: doc.createdAt || now,
+          expiresAt: doc.expiresAt || doc.refreshTokenExpiresAt,
+        }
       } as any;
 
-      const res = await Token.insertOne(toInsert);
+      const filter = {
+        userId: doc.userId,
+        appId: doc.appId,
+        deviceId: doc.deviceId,
+      }
+
+      const res = await Token.updateOne(filter, update, {upsert: true});
       if (!res.acknowledged) throw new SessionCreateError("Failed to insert session document", { res });
       // attach insertedId
-      return { ...toInsert, _id: res.insertedId } as any;
+      return { ...update, _id: res.upsertedId } as any;
     } catch (err) {
       throw new SessionCreateError("Failed to create session", { error: err });
     }
