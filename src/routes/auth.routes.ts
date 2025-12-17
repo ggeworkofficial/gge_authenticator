92a59154ba765a6f1ca2845020c5fe2a6a912ca2
Ezana Tadesse <ezanatadesse2003@gmail.com>
2025-12-17 18:32:08 +0300

    Implemented rate limiting on auth routes.

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -10,16 +10,55 @@ import {
 } from "../validators/auth.validator";
 import { loginController, registerController, refreshController, verifiyController, authenticateAppController, authenticateEndpoint, authenticateMiddleware } from "../controllers/auth.controller";
 import { changePasswordController } from "../controllers/auth.controller";
+import { rateLimiter } from "../middlewares/rateLimiter";
 
 const router = Router();
 
-router.post("/login", authenticateAppController, validateBody(loginSchema), loginController);
-router.post("/register", authenticateAppController, validateBody(registerSchema), registerController);
+router.post(
+  "/login", 
+  rateLimiter({windowSeconds: 60, maxRequests: 5}),
+  authenticateAppController, 
+  validateBody(loginSchema), 
+  loginController
+);
 
-router.post("/authenticate", authenticateAppController, validateBody(authenticateSchema), authenticateEndpoint);
-router.post("/refresh", authenticateAppController, validateBody(refreshSchema), refreshController);
-router.post("/verify", validateBody(verifyCodeSchema), verifiyController);
+router.post(
+  "/register",
+  rateLimiter({ windowSeconds: 60, maxRequests: 3 }),
+  authenticateAppController,
+  validateBody(registerSchema),
+  registerController
+);
 
-router.patch("/change-password", authenticateMiddleware, validateBody(changePasswordSchema), changePasswordController);
+router.post(
+  "/authenticate",
+  rateLimiter({ windowSeconds: 60, maxRequests: 10 }),
+  authenticateAppController,
+  validateBody(authenticateSchema),
+  authenticateEndpoint
+);
+
+router.post(
+  "/refresh",
+  rateLimiter({ windowSeconds: 60, maxRequests: 20 }),
+  authenticateAppController,
+  validateBody(refreshSchema),
+  refreshController
+);
+
+router.post(
+  "/verify",
+  rateLimiter({ windowSeconds: 60, maxRequests: 3 }),
+  validateBody(verifyCodeSchema),
+  verifiyController
+);
+
+router.patch(
+  "/change-password",
+  rateLimiter({ windowSeconds: 60, maxRequests: 2 }),
+  authenticateMiddleware,
+  validateBody(changePasswordSchema),
+  changePasswordController
+);
 //router.post("/verify-email", validateBody(verifiyEmailSchema), verifiyEmailController);
 export default router;
