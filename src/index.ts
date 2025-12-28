f4157287f779edc116ac99a4f82dcd1de6c7767e
Ezana Tadesse <ezanatadesse2003@gmail.com>
2025-12-28 16:06:32 +0300

    Added socket.io connection for real time communication with the client.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import "reflect-metadata";
 import { Logger } from "./utils/logger";
 import { MongoDB } from "./connections/mongodb";
 import { RedisClient } from "./connections/redis";
+import { createServer } from "http";
+import { Server as SocketIOServer } from "socket.io";
 
 const mongodb = MongoDB.getInstance();
 
@@ -35,7 +37,24 @@ async function start() {
   app.use(errorHandler);
 
   const PORT = process.env.PORT;
-  app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
+  const httpServer = createServer(app);
+
+  const io = new SocketIOServer(httpServer, {
+    cors: {
+      origin: "*", // allow your frontend origin here
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    logger.info(`Client connected: ${socket.id}`);
+
+    socket.on("disconnect", () => {
+      logger.info(`Client disconnected: ${socket.id}`);
+    });
+  });
+
+  httpServer.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
 }
 
 start();
